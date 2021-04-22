5ea0441f37c9303e8255ad06d74269a70ba0492d
Christoph <christoph.welling@desy.de>
2021-04-22 11:58:20 +0200

    Add channelSignalReconstructor to dummy event creation and make more fun spectrum

--- dummy_data/create_dummy_data.py
+++ dummy_data/create_dummy_data.py
@@ -3,6 +3,7 @@ import NuRadioReco.framework.event
 import NuRadioReco.framework.station
 import NuRadioReco.framework.channel
 import NuRadioReco.modules.io.eventWriter
+import NuRadioReco.modules.channelSignalReconstructor
 from NuRadioReco.utilities import units
 import argparse
 import astropy.time
@@ -42,16 +43,18 @@ args = parser.parse_args()
 
 event_writer = NuRadioReco.modules.io.eventWriter.eventWriter()
 event_writer.begin(args.filename, events_per_file=args.events_per_file)
+signal_reconstructor = NuRadioReco.modules.channelSignalReconstructor.channelSignalReconstructor()
 
 for i in range(args.n_events):
     event = NuRadioReco.framework.event.Event(0, i)
     station = NuRadioReco.framework.station.Station(args.station_id)
-    for i_channel in range(10):
+    for i_channel in range(24):
         channel = NuRadioReco.framework.channel.Channel(i_channel)
-        spec = np.abs(np.random.normal(0, 1.e-1, args.n_samples // 2 + 1) + 1. / np.sqrt(np.abs(np.arange(args.n_samples // 2 + 1) - 100) + 10))
+        spec = np.abs(np.random.normal(0, 1.e-3 * np.sqrt(i), args.n_samples // 2 + 1) + 1. / np.sqrt(np.abs(np.arange(args.n_samples // 2 + 1) - .0005 * np.square(i) - 50) + 50))
         spec[0] = 0
         channel.set_frequency_spectrum(spec, 1.5 * units.GHz)
         station.add_channel(channel)
     station.set_station_time(astropy.time.Time.now() + astropy.time.TimeDelta(i * .005, format='sec'))
     event.set_station(station)
+    signal_reconstructor.run(event, station, None)
     event_writer.run(event)
