0ed075a49f7a2b7978963b2db8b39858cebe9abd
Steffen Hallmann <steffen.hallmann@desy.de>
2021-06-07 14:01:47 +0200

    modified data provider to read root files per default

--- RNODataViewer/base/data_provider.py
+++ RNODataViewer/base/data_provider.py
@@ -1,17 +1,31 @@
 import NuRadioReco.modules.io.NuRadioRecoio
 import NuRadioReco.utilities.metaclasses
 import six
+from NuRadioReco.modules.io.rno_g.readRNOGData import readRNOGData
 
 @six.add_metaclass(NuRadioReco.utilities.metaclasses.Singleton)
 class RNODataProvider():
-    def __init__(self):
+    def __init__(self, channels=None):
         self.__filenames = None
         self.__event_io = None
+        self.__channels = channels
 
     def set_filenames(self, filenames):
         self.__filenames = filenames
-        self.__event_io = NuRadioReco.modules.io.NuRadioRecoio.NuRadioRecoio(filenames)
+        ## root files, we might add a switch in order to read both?
+        self.__event_io = readRNOGData()
+        self.__event_io.begin(filenames)
+        self.__event_io.run(channels=self.__channels)
+        ## nur files
+        #self.__event_io = NuRadioReco.modules.io.NuRadioRecoio.NuRadioRecoio(filenames)
 
+    def set_iterators(self):
+        ## root files
+        self.__event_io = readRNOGData()
+        self.__event_io.begin(self.__filenames)
+        self.uproot_iterator_header = self.__event_io.uproot_iterator_header
+        self.uproot_iterator_data = self.__event_io.uproot_iterator_data
+    
     def get_event_iterator(self):
         return self.__event_io.get_events
 
