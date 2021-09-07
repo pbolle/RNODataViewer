febc410f668f4724dc4a9055db38f2cadcd6c405
Steffen Hallmann <steffen.hallmann@desy.de>
2021-09-07 16:25:33 +0200

    remove private path

--- RNODataViewer/run_monitor.sh
+++ RNODataViewer/run_monitor.sh
@@ -1,5 +1,7 @@
+#export rnog_eventbrowser branch of NuRadioMC 
 export PYTHONPATH=/Users/shallmann/software/rnog_eventbrowser:$PYTHONPATH
+#export feature/monitoring branch of RNODataViewer
 export PYTHONPATH=/Users/shallmann/software/RNODataViewer_Christoph:$PYTHONPATH
 
 export RNO_DATA_DIR="/Users/shallmann/Desktop/rnog_field_data"
-python /Users/shallmann/software/RNODataViewer_Christoph/RNODataViewer/monitoring.py
+python monitoring.py
